152faadb5e3c847c5adc0d64be7837c6b0a36fad
AntonioTrupac <sniper.trupac@gmail.com>
2021-12-01 21:12:49 +0100

    implemented login/register validation

--- frontend/src/pages/user/Login.tsx
+++ frontend/src/pages/user/Login.tsx
@@ -1,10 +1,13 @@
 import React, { Dispatch, FC, SetStateAction, useCallback } from 'react';
 import { Form, Formik, FormikProps, FormikValues } from 'formik';
+
 import { CustomInput } from '../../components/input/CustomInput';
 import { Button } from '../../components/button/Button';
 import { MutationLoginArgs, useLoginMutation } from '../../generated';
 import { useHistory } from 'react-router-dom';
 import { setAccessToken } from '../../accessToken';
+import { loginValidationSchema } from '../../validation';
+
 type LoginProps = {
    mode: string;
    setMode: Dispatch<SetStateAction<string>>;
@@ -63,31 +66,52 @@ export const Login: FC<LoginProps> = (props) => {
    };
    return (
       <div>
-         <Formik initialValues={initialValues} onSubmit={onSubmit}>
-            {(propsFormik: FormikProps<Values>) => (
+         <Formik
+            initialValues={initialValues}
+            validationSchema={loginValidationSchema}
+            onSubmit={onSubmit}
+         >
+            {({ errors, touched, ...propsFormik }: FormikProps<Values>) => (
                <Form>
                   <div className='login__container'>
-                     <CustomInput
-                        name='email'
-                        placeholder='Email'
-                        value={propsFormik.values.email}
-                        type='text'
-                        className='input'
-                        autoComplete='off'
-                        onChange={propsFormik.handleChange}
-                     />
-                     <CustomInput
-                        name='password'
-                        type='password'
-                        placeholder='Password'
-                        autoComplete='off'
-                        value={propsFormik.values.password}
-                        className='input'
-                        onChange={propsFormik.handleChange}
-                     />
+                     <div className='w-full relative'>
+                        <CustomInput
+                           name='email'
+                           placeholder='Email'
+                           value={propsFormik.values.email}
+                           type='text'
+                           className='input'
+                           autoComplete='off'
+                           onChange={propsFormik.handleChange}
+                        />
+                        {errors.email && touched.email && (
+                           <span className='mt-0 absolute top-7 text-[12px] left-0 text-red-500'>
+                              {errors.email}
+                           </span>
+                        )}
+                     </div>
+
+                     <div className='w-full relative'>
+                        <CustomInput
+                           name='password'
+                           type='password'
+                           placeholder='Password'
+                           autoComplete='off'
+                           value={propsFormik.values.password}
+                           className='input'
+                           onChange={propsFormik.handleChange}
+                        />
+                        {errors.password && touched.password && (
+                           <span className='mt-0 absolute top-7 text-[12px] left-0 text-red-500'>
+                              {errors.password}
+                           </span>
+                        )}
+                     </div>
+
                      <Button type='submit' className='form__button'>
                         Log in
                      </Button>
+
                      <div className='link__container'>
                         <p className='text-[#AFAEAE]'>
                            New to App?{' '}
--- frontend/src/pages/user/Register.tsx
+++ frontend/src/pages/user/Register.tsx
@@ -1,8 +1,10 @@
 import { Dispatch, FC, SetStateAction, useCallback } from 'react';
 import { Form, Formik, FormikProps, FormikValues } from 'formik';
+
 import { CustomInput } from '../../components/input/CustomInput';
 import { Button } from '../../components/button/Button';
 import { RegisterInput, useRegisterMutation } from '../../generated';
+import { registerValidationSchema } from '../../validation';
 
 type RegisterProps = {
    mode: string;
@@ -79,6 +81,7 @@ export const Register: FC<RegisterProps> = (props) => {
       <div>
          <Formik
             initialValues={initialValues}
+            validationSchema={registerValidationSchema}
             onSubmit={(values: FormikValues, { setSubmitting, resetForm }) => {
                setSubmitting(true);
                setTimeout(() => {
@@ -89,60 +92,95 @@ export const Register: FC<RegisterProps> = (props) => {
                }, 2000);
             }}
          >
-            {(propsFormik: FormikProps<Values>) => (
+            {({ errors, touched, ...propsFormik }: FormikProps<Values>) => (
                <Form>
                   <div className='register__container'>
-                     <CustomInput
-                        name='firstName'
-                        type='text'
-                        placeholder='First name'
-                        value={propsFormik.values.firstName}
-                        className='input'
-                        autoComplete='off'
-                        onChange={propsFormik.handleChange}
-                     />
+                     <div className='w-full relative'>
+                        <CustomInput
+                           name='firstName'
+                           type='text'
+                           placeholder='First name'
+                           value={propsFormik.values.firstName}
+                           className='input'
+                           autoComplete='off'
+                           onChange={propsFormik.handleChange}
+                        />
+                        {/* TODO: CREATE A COMPONENT FOR ERROR DISPLAY */}
+                        {errors.firstName && touched.firstName && (
+                           <span className='mt-0 absolute top-7 text-[12px] left-0 text-red-500'>
+                              {errors.firstName}
+                           </span>
+                        )}
+                     </div>
 
-                     <CustomInput
-                        name='lastName'
-                        type='text'
-                        placeholder='Last name'
-                        autoComplete='off'
-                        value={propsFormik.values.lastName}
-                        className='input'
-                        onChange={propsFormik.handleChange}
-                     />
+                     <div className='w-full relative'>
+                        <CustomInput
+                           name='lastName'
+                           type='text'
+                           placeholder='Last name'
+                           autoComplete='off'
+                           value={propsFormik.values.lastName}
+                           className='input'
+                           onChange={propsFormik.handleChange}
+                        />
+                        {errors.lastName && touched.lastName && (
+                           <span className='mt-0 absolute top-7 text-[12px] left-0 text-red-500'>
+                              {errors.lastName}
+                           </span>
+                        )}
+                     </div>
 
-                     <CustomInput
-                        name='phoneNumber'
-                        type='tel'
-                        pattern='[0-9]{3}-[0-9]{3}-[0-9]{4}'
-                        placeholder='Phone number'
-                        autoComplete='off'
-                        value={propsFormik.values.phoneNumber}
-                        className='input'
-                        onChange={propsFormik.handleChange}
-                     />
+                     <div className='w-full relative'>
+                        <CustomInput
+                           name='phoneNumber'
+                           type='tel'
+                           pattern='[0-9]{3}-[0-9]{3}-[0-9]{4}'
+                           placeholder='Phone number'
+                           autoComplete='off'
+                           value={propsFormik.values.phoneNumber}
+                           className='input'
+                           onChange={propsFormik.handleChange}
+                        />
+                        {errors.phoneNumber && touched.phoneNumber && (
+                           <span className='mt-0 absolute top-7 text-[12px] left-0 text-red-500'>
+                              {errors.phoneNumber}
+                           </span>
+                        )}
+                     </div>
 
-                     <CustomInput
-                        name='email'
-                        type='text'
-                        placeholder='Email'
-                        value={propsFormik.values.email}
-                        autoComplete='off'
-                        className='input'
-                        onChange={propsFormik.handleChange}
-                     />
+                     <div className='w-full relative'>
+                        <CustomInput
+                           name='email'
+                           type='text'
+                           placeholder='Email'
+                           value={propsFormik.values.email}
+                           autoComplete='off'
+                           className='input'
+                           onChange={propsFormik.handleChange}
+                        />
+                        {errors.email && touched.email && (
+                           <span className='mt-0 absolute top-7 text-[12px] left-0 text-red-500'>
+                              {errors.email}
+                           </span>
+                        )}
+                     </div>
 
-                     <CustomInput
-                        name='password'
-                        type='password'
-                        placeholder='Password'
-                        autoComplete='off'
-                        value={propsFormik.values.password}
-                        className='input'
-                        onChange={propsFormik.handleChange}
-                     />
-
+                     <div className='w-full relative'>
+                        <CustomInput
+                           name='password'
+                           type='password'
+                           placeholder='Password'
+                           autoComplete='off'
+                           value={propsFormik.values.password}
+                           className='input'
+                           onChange={propsFormik.handleChange}
+                        />
+                        {errors.password && touched.password && (
+                           <span className='mt-0 absolute top-7 text-[12px] left-0 text-red-500'>
+                              {errors.password}
+                           </span>
+                        )}
+                     </div>
                      <Button
                         type='submit'
                         disabled={propsFormik.isSubmitting}
